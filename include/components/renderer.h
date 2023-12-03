2b021c7cfd862dbd661047e2040c308ed7424b98
Squareheron942 <hobbitses417@gmail.com>
2023-12-03 16:50:22 -0500

    Add one last missing file

---
+++ include/components/renderer.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <citro3d.h>
+
+/**
+ * @brief General renderer, abstract class that is only meant to be inherited
+ * 
+ */
+class Renderer {
+    protected:
+    Renderer() {}
+    public:
+        virtual void render(C3D_Mtx& view, C3D_Mtx& projection) = 0;
+};
