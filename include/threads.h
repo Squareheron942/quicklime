76d1040f01a4ffe7378acc5bd23cd1d91502f3f8
square_heron942 <Hobbitses417@gmail.com>
2024-05-25 13:58:56 -0400

    Fix minor mistakes lol

--- include/threads.h
+++ include/threads.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "3ds/synchronization.h"
 #include <3ds.h>
+#include <utility>
 
 struct LightLock_Guard {
 	LightLock& _lock;
@@ -14,9 +14,9 @@ struct LightLock_Mutex {
 	LightLock _lock;
 	T _val;
 	LightLock_Mutex(T&& val): _val(std::move(val)) { LightLock_Init(&_lock); }
-	T& operator->() { return val; }
+	T& operator->() { return _val; }
 	void lock() { LightLock_Lock(&_lock); }
-	unsigned int try_lock() { LightLock_TryLock(&_lock); }
+	unsigned int try_lock() { return LightLock_TryLock(&_lock); }
 	void unlock() { LightLock_Unlock(&_lock); }
 	~LightLock_Mutex() { LightLock_Unlock(&_lock); }
 };
